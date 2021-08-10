9783be3122e134619b7aa73da390749a88ccee7a
Edgar <edgaryp@me.com>
2021-08-10 15:22:29 +1000

    fix: extract payment factory from makePayment

--- typescript/ExtractAndOverrideFactoryMethod/index.ts
+++ typescript/ExtractAndOverrideFactoryMethod/index.ts
@@ -20,7 +20,7 @@ export namespace App {
     }
   }
 
-  class Payment {
+  export class Payment {
     private payer: User;
     private payee: User;
     private cents: number;
@@ -80,6 +80,17 @@ export namespace App {
       this.ledger = new PaymentLedger(this.dbConnection);
     }
 
+    public createPayment(
+      fromName: string,
+      toName: string,
+      cents: number,
+      currency: Currency
+    ): Payment {
+      const fromUser = this.repository.findByName(fromName);
+      const toUser = this.repository.findByName(toName);
+      return new Payment(fromUser, toUser, cents, currency);
+    }
+
     /**
      * Challenge:
      * - Add a currency argument to this method with an appropriate test.
@@ -98,10 +109,7 @@ export namespace App {
       cents: number,
       currency: Currency
     ): void {
-      const fromUser = this.repository.findByName(fromName);
-      const toUser = this.repository.findByName(toName);
-      const payment = new Payment(fromUser, toUser, cents, currency);
-      this.ledger.lodge(payment);
+      this.ledger.lodge(this.createPayment(fromName, toName, cents, currency));
     }
   }
 }
